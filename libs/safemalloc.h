8f931e39cad72f9cf79567e522f61a649ee1748b
Thomas Adam <thomas@fvwm.org>
2019-05-27 00:06:37 +0100

    safemalloc.h: add include guards
    Don't try and include more headers than is necessary by wrapping them in
    an include guard.

--- libs/safemalloc.h
+++ libs/safemalloc.h
@@ -1,6 +1,11 @@
+#ifndef _SAFEMALLOC_H
+#define _SAFEMALLOC_H
+
 void	*xmalloc(size_t);
 void	*xcalloc(size_t, size_t);
 void	*xrealloc(void *, size_t, size_t);
 char	*xstrdup(const char *);
 int	 xasprintf(char **, const char *, ...);
 int	 xvasprintf(char **, const char *, va_list);
+
+#endif
